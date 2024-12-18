ff2dd1d93dbd48146a5e27f355ea0d8b2aee89fa
xichuqing <xichuqing@126.com>
2024-12-18 04:52:48 +0000

    encoder function for encoding categorical variables

---
+++ preprocessing/_preprocessing.py
@@ -0,0 +1,32 @@
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+import sys
+from pathlib import Path
+root =Path(__file__).resolve().parent.parent
+sys.path.append(str(root))
+
+
+def encoder(df):
+    """
+    to encode all needed categorical variables
+    Parameter
+    -----------
+    df: pd.DataFrame
+    """
+    le_sector = preprocessing.LabelEncoder()
+    le_sector.fit(df.Sector)
+    df.Sector = le_sector.transform(df.Sector)  # encode sector
+    print("Sector encoding mapping:", dict(enumerate(le_sector.classes_)))
+    rating_mapping = {
+    'Lowest Risk': 1,
+    'Low Risk': 2,
+    'Medium Risk': 3,
+    'High Risk': 4,
+    'Highest Risk': 5,
+    'In Default': 6
+}
+    # Apply mapping to encode 'Rating'
+    df['Rating'] = df['Rating'].map(rating_mapping)
+    
+    return df
